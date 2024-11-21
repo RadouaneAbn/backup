42744d3ea71829914f2edca4c4cfef89858bc90f
RadouaneAbn <redouane1997.ocs@gmail.com>
2024-11-21 03:15:29 +0100

    [BUG to fix]

--- cursus/get_next_line2/get_next_line.c
+++ cursus/get_next_line2/get_next_line.c
@@ -34,13 +34,14 @@ char *ft_strdup(char *s)
     return (new);
 }
 
-void appendto_line(char **line, t_buf *buffer)
+void appendto_line(char **line, char *buffer)
 {
     int i = 0;
     size_t line_len = ft_strlen(*line);
+    size_t buffer_len = ft_strlen(buffer);
     char *new_line;
 
-    new_line = malloc((line_len + buffer->idx + 1) * sizeof(char));
+    new_line = malloc((line_len + buffer_len + 1) * sizeof(char));
     if (new_line == NULL)
         return ;
     if (*line)
@@ -51,9 +52,9 @@ void appendto_line(char **line, t_buf *buffer)
             i++;
         }
     }
-    while (buffer->str[i])
+    while (buffer[i])
     {
-        new_line[i] = buffer->str[i];
+        new_line[i] = buffer[i];
         i++;
     }
     new_line[i] = '\0';
@@ -62,17 +63,21 @@ void appendto_line(char **line, t_buf *buffer)
     *line = new_line;
 }
 
-int appendto_buffer(char *tmp, t_buf *buffer, char **line)
+void appendto_buffer(char *tmp, t_buf *buffer, char **line)
 {
     int i;
 
     i = 0;
-    while (tmp[i] && tmp[i] != '\n' && buffer->idx < 2047)
-        buffer->str[(buffer->idx)++] = tmp[i++];
+    while (tmp[i] && buffer->idx < 2047)
+    {
+        buffer->str[(buffer->idx)++] = tmp[i];
+        if (tmp[i++] == '\n')
+            break;
+    }
     buffer->str[buffer->idx] = '\0';
     if (tmp[i] && buffer->idx == 2047)
     {
-        appendto_line(line, buffer);
+        appendto_line(line, buffer->str);
         buffer->idx = 0;
         appendto_buffer(tmp + i, buffer, line);
     }
@@ -91,14 +96,13 @@ char *last_clean(char *s, void (*f)(void *))
 {
     int i;
     int j;
-    size_t slen;
     char *last;
 
-    printf(">> cleaning line <<\nline:\n>>>\n%s\n<<<\n", s);
+    if (DM) printf("cleaning line...\n>>>\n%s\n>>>\n<<<\n", s);
     i = 0;
     while (s[i] && s[i] != '\n')
         i++;
-    printf("newline found in %d ascii=[%d]\n", i, s[i]);
+    if (DM) printf("newline found in %d ascii=[%d]\n", i, s[i]);
     if (s[i] == '\0' || s[i + 1] == '\0')
     {
         if (f && s)
@@ -106,17 +110,10 @@ char *last_clean(char *s, void (*f)(void *))
         return (NULL);
     }
     j = i + 1;
-    i = 0;
-    printf("first char after newline: [%c]\n", s[j]);
-    // slen = ft_strlen(s + j) + 1;
-    printf("size malloced: %zu\n", slen);
-    // last = malloc(slen * sizeof(char));
-    // if (last == NULL)
-    //     return (NULL);
-    // while (s[j])
-    //     last[i++] = s[j++];
-    // last[i] = '\0';
     last = ft_strdup(s + j);
+    if (last == NULL)
+        return (NULL);
+    if (DM) printf("%s\n<<<\n\n", last);
     return (last);
 }
 
@@ -128,19 +125,48 @@ char *get_next_line(int fd)
     ssize_t size;
     char *line;
 
+    if (DM) printf("read from: %d\n", fd);
+
+    if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, b_tmp, 0) == -1)
+        return (NULL);
     buffer.idx = 0;
     line = NULL;
+    if (DM) printf("last line: %s\n", last);
+    if (last)
+    {
+        if (nl_found(last))
+        {
+            if (DM) printf("last -> nl\n");
+            // appendto_line(&line, last);
+            appendto_buffer(last, &buffer, &line);
+            appendto_line(&line, buffer.str);
+            last = last_clean(last, free);
+        }
+        else
+        {
+            if (DM) printf("last -> nonl\n");
+            appendto_buffer(last, &buffer, &line);
+            free(last);
+            last = NULL;
+        }
+    }
     if (last == NULL)
     {
         while ((size = read(fd, b_tmp, BUFFER_SIZE)) > 0)
         {
+            if (DM) printf("size read: %ld\n", size);
             b_tmp[size] = '\0';
+            if (DM) printf("read: %s\n", b_tmp);
             appendto_buffer(b_tmp, &buffer, &line);
             if (nl_found(b_tmp))
                 break;
         }
-        appendto_line(&line, &buffer);
-        printf("> result: %s\n", line);
+        if (DM) printf("size read: %ld\n", size);
+        if (DM) printf("buf idx: %zu\n", buffer.idx);
+        if (size <= 0 && buffer.idx == 0)
+            return (NULL);
+        appendto_line(&line, buffer.str);
+        if (DM) printf("tmp: %s\n", b_tmp);
         last = last_clean(b_tmp, NULL);
     }
     return (line);
--- cursus/get_next_line2/get_next_line.h
+++ cursus/get_next_line2/get_next_line.h
@@ -2,13 +2,18 @@
 # define GET_NEXT_LINE
 
 # ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 1024
+#  define BUFFER_SIZE 10
 # endif
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
+
+
+# ifndef DM
+#  define DM 0
+# endif
 
 typedef struct s_buf {
     char str[2048];
