44762f1b22cbd688725c7d1bff18547d4e906d9e
RadouaneAbn <radouaneabn@gmail.com>
2024-11-28 21:08:51 +0100

    going for the bonus

---
+++ cursus/ft_printf/format.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    char *n = NULL;
+    char *s = "red";
+
+    printf("[%5d]\n", 42); // left padding with 5
+    printf("[%-5d]\n", 42); // right padding with 5
+
+    printf("[%.5d]\n", 42); // left padding with 5 (filled with 0)
+
+    printf("[%x]\n", 42);
+    printf("[%#x]\n", 42); // adds 0x before the hexa number
+
+    printf("[%+d]\n", 42); // prints the sign -/+
+
+    printf("[% d]\n", 42); // prints space instead of the + sign
+    printf("[% d]\n", -42); // prints the - sign
+
+    // `-`: left justify (right padding)
+    // `0`: pad with 0
+    // `.`: precision for number (float / int(minimum digits))
+    // `#`: alternative format (hex: 0x)
+    // `+`: always show the sign
+    // ` `: show leading space for positive integers
+}
