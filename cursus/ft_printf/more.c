6a77ce123f3a6622410cf4cd376a1d1783236929
RadouaneAbn <radouaneabn@gmail.com>
2024-12-02 20:46:22 +0100

    bunus str

--- cursus/ft_printf/more.c
+++ cursus/ft_printf/more.c
@@ -90,52 +90,6 @@ int	print_precision(int precision)
 	return (count);
 }
 
-
-// int ft_putstr_2(char *s, t_opt *opt, char filler)
-// {
-//     int     slen;
-//     int     count;
-//     char    *new_s;
-// 	int		sign;
-
-//     if (s == NULL)
-//         new_s = "(null)";
-//     else
-//         new_s = s;
-
-//     slen = ft_strlen(new_s);
-//     count = 0;
-//     sign = 0;
-//     // Print string with necessary precision
-// 	if (char_in_chaset(*new_s, "-+ "))
-// 	{
-// 		sign = 1;
-// 		slen--;
-// 	}
-
-//     // Adjust width based on precision
-//     if (opt->precision[1] >= slen)
-//         opt->width -= opt->precision[1];
-//     else
-//         opt->width -= slen;
-
-//     if (opt->width < 0)
-//         opt->width = 0;
-
-//     // Print filler before string if needed
-//     count += print_filler(filler, opt->pad == 0, opt->width);
-
-// 	if (sign && opt->fill == '0')
-// 		count += ft_putchar(*new_s++);
-//     count += print_precision(opt->precision[1] - slen);
-//     count += write(1, new_s, slen);
-
-//     // Print filler after string if needed
-//     count += print_filler(filler, opt->pad == 1, opt->width);
-
-//     return count;
-// }
-
 int	ft_putstr_2(char *s, t_opt *opt, char filler)
 {
 	int		slen;
@@ -162,12 +116,9 @@ int	ft_putstr_2(char *s, t_opt *opt, char filler)
 	if (opt->width < 0)
 		opt->width = 0;
 	if (sign && opt->fill == '0')
-	{
 		count += ft_putchar(*new_s++);
-		sign = 0;
-	}
 	count += print_filler(filler, opt->pad == 0, opt->width);
-	if (sign && (opt->precision[0]))
+	if (sign && opt->fill == ' ')
 		count += ft_putchar(*new_s++);
 	count += print_precision(opt->precision[1] - slen);
 	count += write(1, new_s, slen);
