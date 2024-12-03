f64ebb259847649e6664dc0a2635678b02b7a714
RadouaneAbn <radouaneabn@gmail.com>
2024-12-03 15:03:42 +0100

    bonus

--- cursus/ft_printf/src/printing_funcs.c
+++ cursus/ft_printf/src/printing_funcs.c
@@ -1,14 +1,15 @@
 #include "../ft_printf.h"
 
-// int print_all(int *count, t_opt *opt, char *s, int slen)
-// {
-// 	if (print_filler(count, opt->fill, opt->pad == 0, opt->width - slen) == -1)
-// 		return (-1);
-// 	if (writer(count, s, slen) == -1)
-// 		return (-1);
-// 	if (print_filler(count, opt->fill, opt->pad == 1, opt->width - slen) == -1)
-// 		return (-1);
-// }
+int print_all(int *count, t_opt *opt, char *s, int slen)
+{
+	if (print_filler(count, opt->fill, opt->pad == 0, opt->width - slen) == -1)
+		return (-1);
+	if (writer(count, s, slen) == -1)
+		return (-1);
+	if (print_filler(count, opt->fill, opt->pad == 1, opt->width - slen) == -1)
+		return (-1);
+	return (*count);
+}
 
 int	print_filler(int *count, char c, int cond, int n)
 {
@@ -35,13 +36,14 @@ int	ft_putaddr(char *s, t_opt *opt)
 		new_s = s;
 	slen = ft_strlen(new_s);
 	count = 0;
-
-	if (print_filler(&count, opt->fill, opt->pad == 0, opt->width - slen) == -1)
+	if (print_all(&count, opt, new_s, slen) == -1)
 		return (-1);
-	if (writer(&count, s, slen) == -1)
-		return (-1);
-	if (print_filler(&count, opt->fill, opt->pad == 1, opt->width - slen) == -1)
-		return (-1);
+	// if (print_filler(&count, opt->fill, opt->pad == 0, opt->width - slen) == -1)
+	// 	return (-1);
+	// if (writer(&count, s, slen) == -1)
+	// 	return (-1);
+	// if (print_filler(&count, opt->fill, opt->pad == 1, opt->width - slen) == -1)
+	// 	return (-1);
 	return (count);
 }
 
@@ -64,12 +66,14 @@ int	ft_putstr(char *s, t_opt *opt)
 	if (opt->precision[0] && opt->precision[1] < slen)
 		slen = opt->precision[1];
 	count = 0;
-	if (print_filler(&count, opt->fill, opt->pad == 0, opt->width - slen) == -1)
+	if (print_all(&count, opt, new_s, slen) == -1)
 		return (-1);
-	if (writer(&count, new_s, slen) == -1)
-		return (-1);
-	if (print_filler(&count, opt->fill, opt->pad == 1, opt->width - slen) == -1)
-		return (-1);
+	// if (print_filler(&count, opt->fill, opt->pad == 0, opt->width - slen) == -1)
+	// 	return (-1);
+	// if (writer(&count, new_s, slen) == -1)
+	// 	return (-1);
+	// if (print_filler(&count, opt->fill, opt->pad == 1, opt->width - slen) == -1)
+	// 	return (-1);
 	return (count);
 }
 
@@ -82,6 +86,23 @@ int	print_precision(int *count, int precision)
 		precision--;
 	}
 	return (*count);
+}
+
+void calculate_width_precision(char *s, t_opt *opt, int *slen, int *sign)
+{
+	if (opt->precision[0] && opt->precision[1] == 0 && s[0] == '0')
+		*slen = 0;
+	if (char_in_chaset(*s, "-+ "))
+	{
+		*sign = 1;
+		(*slen)--;
+	}
+	if (opt->precision[1] >= *slen)
+		opt->width -= opt->precision[1] + *sign;
+	else
+		opt->width -= *slen + *sign;
+	if (opt->width < 0)
+		opt->width = 0;
 }
 
 int	ft_putnbr(char *s, t_opt *opt)
@@ -98,19 +119,21 @@ int	ft_putnbr(char *s, t_opt *opt)
 	slen = ft_strlen(new_s);
 	count = 0;
 	sign = 0;
-	if (opt->precision[0] && opt->precision[1] == 0 && new_s[0] == '0')
-		slen = 0;
-	if (char_in_chaset(*new_s, "-+ "))
-	{
-		sign = 1;
-		slen--;
-	}
-	if (opt->precision[1] >= slen)
-		opt->width -= opt->precision[1] + sign;
-	else
-		opt->width -= slen + sign;
-	if (opt->width < 0)
-		opt->width = 0;
+	calculate_width_precision(new_s, opt, &slen, &sign);
+	// if (opt->precision[0] && opt->precision[1] == 0 && new_s[0] == '0')
+	// 	slen = 0;
+	// if (char_in_chaset(*new_s, "-+ "))
+	// {
+	// 	sign = 1;
+	// 	slen--;
+	// }
+	// if (opt->precision[1] >= slen)
+	// 	opt->width -= opt->precision[1] + sign;
+	// else
+	// 	opt->width -= slen + sign;
+	// if (opt->width < 0)
+	// 	opt->width = 0;
+
 	if (sign && opt->fill == '0')
 		count += ft_putchar(*new_s++);
 	if (print_filler(&count, opt->fill, opt->pad == 0, opt->width) == -1 )
