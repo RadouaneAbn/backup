e501a4ed6f892a42a989d89d69078dcaf8dedf82
RadouaneAbn <radouaneabn@gmail.com>
2025-11-24 01:32:58 +0100

    ex02

--- cursus/cpp02/ex02/Fixed.cpp
+++ cursus/cpp02/ex02/Fixed.cpp
@@ -3,130 +3,196 @@
 
 
 Fixed::Fixed( void ) {
-    _fixedPoint = 0;
+	_fixedPoint = 0;
 }
 
 Fixed::Fixed ( const Fixed &n ) {
-    this->_fixedPoint = n._fixedPoint;
+	this->_fixedPoint = n._fixedPoint;
 }
 
 Fixed& Fixed::operator= ( const Fixed& n ) {
-    if (this == &n)
-        return (*this);
-    this->_fixedPoint = n._fixedPoint;
-    return (*this);
+	if (this == &n)
+		return (*this);
+	this->_fixedPoint = n._fixedPoint;
+	return (*this);
 }
 
 Fixed::~Fixed( void ) {
 }
 
 int Fixed::getRawBits( void ) const {
-    return (_fixedPoint);
+	return (_fixedPoint);
 }
 
 void Fixed::setRawBits( int const raw ) {
-    _fixedPoint = raw;
+	_fixedPoint = raw;
 }
 
 Fixed::Fixed( const int n ) {
-    _fixedPoint = (n * (1 << _fractionalBits));
+	_fixedPoint = (n * (1 << _fractionalBits));
 }
 
 Fixed::Fixed( const float n ) {
-    _fixedPoint = roundf(n * (1 << _fractionalBits));
+	_fixedPoint = roundf(n * (1 << _fractionalBits));
 }
 
 
 int Fixed::toInt( void ) const {
-    return (_fixedPoint / int(1 << _fractionalBits));
+	return (_fixedPoint / int(1 << _fractionalBits));
 } 
 
 float Fixed::toFloat( void ) const {
-    return (float(_fixedPoint) / float(1 << _fractionalBits));
+	return (float(_fixedPoint) / float(1 << _fractionalBits));
 }
 
 bool Fixed::operator> ( const Fixed& n ) const {
-    return (this->getRawBits() > n.getRawBits());
+	return (this->getRawBits() > n.getRawBits());
 }
 
 bool Fixed::operator< ( const Fixed& n ) const {
-    return (this->getRawBits() < n.getRawBits());
+	return (this->getRawBits() < n.getRawBits());
 }
 
 bool Fixed::operator>= ( const Fixed& n ) const {
-    return (this->getRawBits() >= n.getRawBits());
+	return (this->getRawBits() >= n.getRawBits());
 }
 
 bool Fixed::operator<= ( const Fixed& n ) const {
-    return (this->getRawBits() <= n.getRawBits());
+	return (this->getRawBits() <= n.getRawBits());
 }
 
 bool Fixed::operator== ( const Fixed& n ) const {
-    return (this->getRawBits() == n.getRawBits());
+	return (this->getRawBits() == n.getRawBits());
 }
 
 bool Fixed::operator!= ( const Fixed& n ) const {
-    return (this->getRawBits() != n.getRawBits());
+	return (this->getRawBits() != n.getRawBits());
 }
+
+/**
+ * A_real = A_raw / 256
+ * B_real = B_raw / 256
+ * result_real = A_real + B_real
+ *             = (A_raw / 256) + (B_raw / 256)
+ *             = (A_raw + B_raw) / 256
+ * 
+ * result_raw = result_real * 256
+ *            = ((A_raw + B_raw) / 256) * 256
+ * <==>
+ * result_raw = A_raw + B_raw
+ */
 
 Fixed Fixed::operator+ ( const Fixed& n ) const {
-    return (Fixed(this->toFloat() + n.toFloat()));
+	Fixed new_fixed;
+	new_fixed.setRawBits(this->getRawBits() + n.getRawBits());
+	return (new_fixed);
 }
+
+/**
+ * A_real = A_raw / 256
+ * B_real = B_raw / 256
+ * result_real = A_real - B_real
+ *             = (A_raw / 256) - (B_raw / 256)
+ *             = (A_raw - B_raw) / 256
+ * 
+ * result_raw = result_real * 256
+ *            = ((A_raw - B_raw) / 256) * 256
+ * <==>
+ * result_raw = A_raw - B_raw
+ */
 
 Fixed Fixed::operator- ( const Fixed& n ) const {
-    return (Fixed(this->toFloat() - n.toFloat()));
+	Fixed new_fixed;
+	new_fixed.setRawBits(this->getRawBits() - n.getRawBits());
+	return (new_fixed);
 }
+
+/**
+ * A_real = A_raw / 256
+ * B_real = B_raw / 256
+ *
+ * result_real = A_real * B_real
+ * <==>
+ *             = (A_raw/256) * (B_raw/256)
+ *             = (A_raw * B_raw) / 65536
+ * ==>
+ * result_raw = result_real * 256
+ *            = ((A_raw * B_raw) / 65536) * 256
+ *            = (A_raw * B_raw) / 256
+ * <==>
+ * result_raw = (A_raw * B_raw) >> 8
+ */
 
 Fixed Fixed::operator* ( const Fixed& n ) const {
-    return (Fixed(this->toFloat() * n.toFloat()));
+	Fixed new_fixed;
+	new_fixed.setRawBits((this->getRawBits() * n.getRawBits()) >> _fractionalBits);
+	return (new_fixed);
 }
 
+/**
+ * A_real = A_raw / 256
+ * B_real = B_raw / 256
+ *
+ * result_real = A_real * B_real
+ * <==>
+ *             = (A_raw/256) / (B_raw/256)
+ *             = A_raw / B_raw
+ * ==>
+ * result_raw = result_real * 256
+ *            = (A_raw / B_raw) * 256
+ *            = (A_raw * 256) / B_raw
+ * <==>
+ * result_raw = (A_raw << 8) / B_raw
+ */
+
 Fixed Fixed::operator/ ( const Fixed& n ) const {
-    return (Fixed(this->toFloat() / n.toFloat()));
+	Fixed new_fixed;
+	new_fixed.setRawBits((this->getRawBits() << _fractionalBits) / n.getRawBits());
+	return (new_fixed);
 }
 
 Fixed& Fixed::operator++ ( void ) {
-    _fixedPoint++;
-    return (*this);
+	_fixedPoint++;
+	return (*this);
 }
 
 Fixed& Fixed::operator-- ( void ) {
-    _fixedPoint--;
-    return (*this);
+	_fixedPoint--;
+	return (*this);
 }
 
 Fixed Fixed::operator++ ( int ) {
-    Fixed tmp = *this;
-    _fixedPoint++;
-    return (tmp);
+	Fixed tmp = *this;
+	_fixedPoint++;
+	return (tmp);
 }
 
 Fixed Fixed::operator-- ( int ) {
-    Fixed tmp = *this;
-    _fixedPoint--;
-    return (tmp);
+	Fixed tmp = *this;
+	_fixedPoint--;
+	return (tmp);
 }
 
 Fixed& Fixed::min (Fixed& a, Fixed& b) {
-    if (a.toFloat() <= b.toFloat())
-        return (a);
-    return (b);
+	if (a.toInt() <= b.toInt())
+		return (a);
+	return (b);
 }
 
 Fixed& Fixed::max (Fixed& a, Fixed& b) {
-    if (a.toFloat() >= b.toFloat())
-        return (a);
-    return (b);
+	if (a.toInt() >= b.toInt())
+		return (a);
+	return (b);
 }
 
 const Fixed& Fixed::min (const Fixed& a,const Fixed& b) {
-    if (a.toFloat() <= b.toFloat())
-        return (a);
-    return (b);
+	if (a.toInt() <= b.toInt())
+		return (a);
+	return (b);
 }
 
 const Fixed& Fixed::max (const Fixed& a,const Fixed& b) {
-    if (a.toFloat() >= b.toFloat())
-        return (a);
-    return (b);
+	if (a.toInt() >= b.toInt())
+		return (a);
+	return (b);
 }
