a475ee7a64fbda0bf60cebf9b5e993eb82ba0ec0
RadouaneAbn <radouaneabn@gmail.com>
2026-01-19 23:53:15 +0100

    ex02

---
+++ cursus/cpp08/ex02/MutantStack.hpp
@@ -0,0 +1,28 @@
+#ifndef MUTANTSTACK_HPP
+#define MUTANTSTACK_HPP
+
+#include <iostream>
+#include <stack>
+
+template <typename T>
+class MutantStack : public std::stack<T>
+{
+public:
+
+    typedef typename std::stack<T>::container_type::iterator iterator; 
+    typedef typename std::stack<T>::container_type::const_iterator const_iterator;
+    MutantStack(void);
+    ~MutantStack(void);
+    MutantStack(const MutantStack &oldObj);
+    MutantStack &operator=(const MutantStack &oldObj);
+
+    iterator begin();
+    iterator end();
+
+    const_iterator begin() const;
+    const_iterator end() const;
+};
+
+#include "MutantStack.tpp"
+
+#endif
